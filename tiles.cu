c79bddc149f92c09af3e71ef58ab1530edc33e69
Roland Haas <rhaas@illinois.edu>
2021-11-10 17:41:03 -0600

    wip

--- tiles.cu
+++ tiles.cu
@@ -5,13 +5,19 @@
 
 #define TILE_SX 32
 #define TILE_SY 32
+#define TILES_X 10
+#define TILES_Y 10
 
-double __device__ dx(const double * v) {
+float __device__ dx(const float * v) {
     return 0.5*(v[+1] - v[-1]);
 }
 
-double __device__ dy(const double * v, const int dj) {
+float __device__ dy(const float * v, const int dj) {
     return 0.5*(v[dj] - v[-dj]);
+}
+
+float __device__ dxy(const float * v, const int dj) {
+    return 0.25*((v[dj+1]-v[dj-1]) - (v[-dj+1] - v[-dj-1]));
 }
 
 #define CUDA_CHECK do { \
@@ -22,65 +28,106 @@ double __device__ dy(const double * v, const int dj) {
   } \
 } while(0) 
 
-void __global__ derivs(const double * v, double *v_x, double *v_y, double *v_xy) {
-  double __shared__ tile[TILE_SX * TILE_SY];
-  double __shared__ tile_x[TILE_SX * TILE_SY];
+void __global__ derivs(const float * v, float *v_x, float *v_y, float *v_xy) {
+  float __shared__ tile[TILE_SX * TILE_SY];
+  float __shared__ tile_x[TILE_SX * TILE_SY];
 
-  int idx = threadIdx.x + threadIdx.y*TILE_SX;
-  tile[idx] = v[idx]; 
+  int tidx = threadIdx.x + threadIdx.y*TILE_SX;
+  int idx = threadIdx.x + threadIdx.y*TILE_SX + blockIdx.x*TILE_SX*TILE_SY 
+            + blockIdx.y*TILES_X*TILE_SX*TILE_SY;
+  tile[tidx] = v[idx]; 
   __syncthreads();
   
   if(threadIdx.x > 0 && threadIdx.x < TILE_SX - 1) {
-    v_x[idx] = tile_x[idx] = dx(&tile[idx]);
+    v_x[idx] = tile_x[tidx] = dx(&tile[tidx]);
   }
 
   __syncthreads();
 
-  if(threadIdx.y > 0 && threadIdx.y < TILE_SX - 1) {
-    v_y[idx] = dy(&tile[idx], TILE_SY);
-    v_xy[idx] = dy(&tile_x[idx], TILE_SY);
+  if(threadIdx.y > 0 && threadIdx.y < TILE_SY - 1) {
+    v_y[idx] = dy(&tile[tidx], TILE_SY);
+    v_xy[idx] = dy(&tile_x[tidx], TILE_SY);
   }
 }
 
-double now() {
+void __global__ derivs_naive(const float * v, float *v_x, float *v_y, float *v_xy) {
+  int idx = threadIdx.x + threadIdx.y*TILE_SX + blockIdx.x*TILE_SX*TILE_SY 
+            + blockIdx.y*TILES_X*TILE_SX*TILE_SY;
+  
+  if(threadIdx.x > 0 && threadIdx.x < TILE_SX - 1 &&
+     threadIdx.y > 0 && threadIdx.y < TILE_SY - 1) {
+    v_x[idx] = dx(&v[idx]);
+    v_y[idx] = dy(&v[idx], TILE_SY);
+    v_xy[idx] = dxy(&v[idx], TILE_SY);
+  }
+}
+
+float now() {
   timeval t;
   gettimeofday(&t, NULL);
   return t.tv_sec + t.tv_usec*1e-6;
 }
 
 int main(void) {
-  double *v = (double*)calloc(TILE_SX * TILE_SY, sizeof(*v));
-  double *v_x = (double*)calloc(TILE_SX * TILE_SY, sizeof(*v_x));
-  double *v_y = (double*)calloc(TILE_SX * TILE_SY, sizeof(*v_y));
-  double *v_xy = (double*)calloc(TILE_SX * TILE_SY, sizeof(*v_xy));
-  double *d_v, *d_v_x, *d_v_y, *d_v_xy;
-  cudaMalloc(&d_v, TILE_SX * TILE_SY * sizeof(*v));
+  float *v = (float*)calloc(TILE_SX * TILE_SY * TILES_X * TILES_Y, sizeof(*v));
+  float *v_x = (float*)calloc(TILE_SX * TILE_SY * TILES_X * TILES_Y, sizeof(*v_x));
+  float *v_y = (float*)calloc(TILE_SX * TILE_SY * TILES_X * TILES_Y, sizeof(*v_y));
+  float *v_xy = (float*)calloc(TILE_SX * TILE_SY * TILES_X * TILES_Y, sizeof(*v_xy));
+  float *d_v, *d_v_x, *d_v_y, *d_v_xy;
+  cudaMalloc(&d_v, TILE_SX * TILE_SY * TILES_X * TILES_Y * sizeof(*v));
   CUDA_CHECK;
-  cudaMalloc(&d_v_x, TILE_SX * TILE_SY * sizeof(*v_x));
+  cudaMalloc(&d_v_x, TILE_SX * TILE_SY * TILES_X * TILES_Y * sizeof(*v_x));
   CUDA_CHECK;
-  cudaMalloc(&d_v_y, TILE_SX * TILE_SY * sizeof(*v_y));
+  cudaMalloc(&d_v_y, TILE_SX * TILE_SY * TILES_X * TILES_Y * sizeof(*v_y));
   CUDA_CHECK;
-  cudaMalloc(&d_v_xy, TILE_SX * TILE_SY * sizeof(*v_xy));
+  cudaMalloc(&d_v_xy, TILE_SX * TILE_SY * TILES_X * TILES_Y * sizeof(*v_xy));
   CUDA_CHECK;
 
+  // disable caches for testing
+  cudaDeviceSetCacheConfig(cudaFuncCachePreferShared);
+
+  for(int j=0;j<2;j++) {
+  {
+    cudaMemcpy(d_v, v, TILE_SX * TILE_SY * TILES_X * TILES_Y * sizeof(*v), cudaMemcpyHostToDevice);
+    CUDA_CHECK;
+    dim3 dimBlock(TILE_SX, TILE_SY, 1);
+    dim3 dimGrid(TILES_X, TILES_Y, 1);
   double start = now();
   // TODO: run one without timint to get rid of initialization ost
   for(int i = 0 ; i < 1000 ; i++) {
-    cudaMemcpy(d_v, v, TILE_SX * TILE_SY * sizeof(*v), cudaMemcpyHostToDevice);
-    CUDA_CHECK;
-    dim3 dimBlock(TILE_SX, TILE_SY);
-    dim3 dimGrid(1, 1);
     derivs<<<dimGrid, dimBlock>>>(d_v, d_v_x, d_v_y, d_v_xy);
-    CUDA_CHECK;
-    cudaMemcpy(v_x, d_v_x, TILE_SX * TILE_SY * sizeof(*v_x), cudaMemcpyDeviceToHost);
-    CUDA_CHECK;
-    cudaMemcpy(v_y, d_v_y, TILE_SX * TILE_SY * sizeof(*v_y), cudaMemcpyDeviceToHost);
-    CUDA_CHECK;
-    cudaMemcpy(v_xy, d_v_xy, TILE_SX * TILE_SY * sizeof(*v_xy), cudaMemcpyDeviceToHost);
-    CUDA_CHECK;
   }
   double end = now();
-  std::cout << "took " << (end-start) << " s\n";
+  std::cout << "tiled took " << (end-start) << " s\n";
+    CUDA_CHECK;
+    cudaMemcpy(v_x, d_v_x, TILE_SX * TILE_SY * TILES_X * TILES_Y * sizeof(*v_x), cudaMemcpyDeviceToHost);
+    CUDA_CHECK;
+    cudaMemcpy(v_y, d_v_y, TILE_SX * TILE_SY * TILES_X * TILES_Y * sizeof(*v_y), cudaMemcpyDeviceToHost);
+    CUDA_CHECK;
+    cudaMemcpy(v_xy, d_v_xy, TILE_SX * TILE_SY * TILES_X * TILES_Y * sizeof(*v_xy), cudaMemcpyDeviceToHost);
+    CUDA_CHECK;
+  }
+  {
+    cudaMemcpy(d_v, v, TILE_SX * TILE_SY * TILES_X * TILES_Y * sizeof(*v), cudaMemcpyHostToDevice);
+    CUDA_CHECK;
+    dim3 dimBlock(TILE_SX, TILE_SY);
+    dim3 dimGrid(TILES_X, TILES_Y);
+  double start = now();
+  // TODO: run one without timint to get rid of initialization ost
+  for(int i = 0 ; i < 1000 ; i++) {
+    derivs_naive<<<dimGrid, dimBlock>>>(d_v, d_v_x, d_v_y, d_v_xy);
+  }
+  double end = now();
+  std::cout << "naive took " << (end-start) << " s\n";
+    CUDA_CHECK;
+    cudaMemcpy(v_x, d_v_x, TILE_SX * TILE_SY * TILES_X * TILES_Y * sizeof(*v_x), cudaMemcpyDeviceToHost);
+    CUDA_CHECK;
+    cudaMemcpy(v_y, d_v_y, TILE_SX * TILE_SY * TILES_X * TILES_Y * sizeof(*v_y), cudaMemcpyDeviceToHost);
+    CUDA_CHECK;
+    cudaMemcpy(v_xy, d_v_xy, TILE_SX * TILE_SY * TILES_X * TILES_Y * sizeof(*v_xy), cudaMemcpyDeviceToHost);
+    CUDA_CHECK;
+  }
+  }
 
   return 0;
 }
